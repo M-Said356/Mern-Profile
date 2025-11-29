69c088d93f1bcb56d813b83cdc5022cb65e3c68f
Mostafa-SAID7 <mostafa-said7@github.com>
2025-11-29 13:01:49 +0200

    fully part

---
+++ backend/app.js
@@ -0,0 +1,62 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import fileUpload from "express-fileupload";
+import cors from "cors";
+import { dbConnection } from "./database/connection.js";
+import { errorMiddleware } from "./middlewares/error.js";
+import userRouter from "./routes/userRouter.js";
+import timelineRouter from "./routes/timelineRouter.js";
+import messageRouter from "./routes/messageRouter.js";
+import skillRouter from "./routes/skillRouter.js";
+import softwareApplicationRouter from "./routes/softwareApplicationRouter.js";
+import projectRouter from "./routes/projectRouter.js";
+
+const app = express();
+dotenv.config({ path: "./config/config.env" });
+
+app.use(
+  cors({
+    origin: [process.env.PORTFOLIO_URL, process.env.DASHBOARD_URL],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
+  })
+);
+
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "API is running successfully!",
+    endpoints: {
+      user: "/api/v1/user",
+      timeline: "/api/v1/timeline",
+      message: "/api/v1/message",
+      skill: "/api/v1/skill",
+      softwareApplication: "/api/v1/softwareapplication",
+      project: "/api/v1/project"
+    }
+  });
+});
+
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/timeline", timelineRouter);
+app.use("/api/v1/message", messageRouter);
+app.use("/api/v1/skill", skillRouter);
+app.use("/api/v1/softwareapplication", softwareApplicationRouter);
+app.use("/api/v1/project", projectRouter);
+
+dbConnection();
+app.use(errorMiddleware);
+
+export default app;
